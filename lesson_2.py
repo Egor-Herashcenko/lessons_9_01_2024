791d1d0ae70200ba31872f679d4563ce4b0cf92f
Egor-Herashcenko <deagmace3@gmail.com>
2024-01-13 13:09:39 +0200

    second commit

--- lesson_2.py
+++ lesson_2.py
@@ -1,10 +1,10 @@
-a = 9743
-a4 = a % 10
-a3 = a // 10 % 10
-a2 = a // 100 % 10
-a1 = a // 1000
+number = int(input())
+thousandths = number % 10
+hundredths = number // 10 % 10
+tenths = number // 100 % 10
+units = number // 1000
 
-print(a1)
-print(a2)
-print(a3)
-print(a4)
+print(units)
+print(tenths)
+print(hundredths)
+print(thousandths)
