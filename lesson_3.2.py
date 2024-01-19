3097d947939f4125af3b01159c9f0605900d92cd
Egor-Herashcenko <deagmace3@gmail.com>
2024-01-19 22:08:09 +0200

    second commit

--- lesson_3.2.py
+++ lesson_3.2.py
@@ -1,4 +1,4 @@
-value_list = [1, 4, 5, 6]
+value_list = [1, 2, 5, 7, 8, 10]
 if value_list:
-    value_list.insert(0, value_list.pop(-1))
+    value_list.insert(0, value_list.pop())
 print(value_list)
