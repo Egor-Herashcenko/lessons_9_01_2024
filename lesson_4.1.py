8b38b786996368be6543614401644014e845d694
Egor-Herashcenko <deagmace3@gmail.com>
2024-01-20 14:51:40 +0200

    second commit

---
+++ lesson_4.1.py
@@ -0,0 +1,12 @@
+# value_list = [0, 4, 5, 0, 6, 0, 7]
+# value_list.sort(key=lambda x: x == 0)
+# print(value_list)
+
+
+value_list = [1, 0, 3, 0, 0, 0, 5, 6, 0, 8, 0, 3]
+for zero in range(len(value_list) - 1, -1, -1):
+   if value_list[zero] == 0:
+      value_list.append(value_list.pop(zero))
+
+print(value_list)
+
---
+++ lesson_4.2.py
@@ -0,0 +1,3 @@
+value_list = []
+print(sum(value_list[::2]) * value_list[-1]
+      if value_list else 0)
